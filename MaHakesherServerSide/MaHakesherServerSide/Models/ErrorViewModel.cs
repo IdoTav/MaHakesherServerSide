3079cc19544c57cf399dda732f0a5f43d19b2867
Ido David Tavron <idodavid.tavron@live.biu.ac.il>
2024-01-29 14:54:24 +0200

    starting a .net project

---
+++ MaHakesherServerSide/MaHakesherServerSide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaHakesherServerSide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
